b381bcd778fcca4084ebe6326c76100c29ce73e8
Maxim S <deed34@yandex.ru>
2024-08-10 15:02:56 +0300

    fix: remove loading state from header profile-section

--- src/app/(main)/_components/header/profile-section.tsx
+++ src/app/(main)/_components/header/profile-section.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { logout as logoutServerAction } from "@app/actions/auth";
-import { ActionIcon, Anchor, Avatar, Group, Skeleton } from "@mantine/core";
+import { ActionIcon, Anchor, Avatar, Group } from "@mantine/core";
 import { useAppDispatch, useAppSelector } from "@store/hooks";
 import { logout } from "@store/reducers/user";
 import { IconLogout } from "@tabler/icons-react";
@@ -13,7 +13,7 @@ import styles from "./styles.module.scss";
 
 export const ProfileSection = (): JSX.Element => {
   const router = useRouter();
-  const user = useAppSelector((state) => state.user);
+  const userData = useAppSelector((state) => state.user.data);
   const dispatch = useAppDispatch();
 
   const handleLogout = (): void => {
@@ -28,28 +28,19 @@ export const ProfileSection = (): JSX.Element => {
   return (
     <Group gap="lg">
       <Group align="center" gap={8}>
-        {user.data ? (
-          <>
-            <Avatar color="pink" radius="xl" size={28}>
-              {user.data.username[0]}
-            </Avatar>
-            <Anchor
-              fw="bold"
-              size="sm"
-              underline="never"
-              component={Link}
-              href="/me"
-              className={styles.profileLink}
-            >
-              {user.data.username}
-            </Anchor>
-          </>
-        ) : (
-          <>
-            <Skeleton height={28} circle />
-            <Skeleton height={12} width={80} radius="xl" />
-          </>
-        )}
+        <Avatar color="pink" radius="xl" size={28}>
+          {userData?.username[0]}
+        </Avatar>
+        <Anchor
+          fw="bold"
+          size="sm"
+          underline="never"
+          component={Link}
+          href="/me"
+          className={styles.profileLink}
+        >
+          {userData?.username}
+        </Anchor>
       </Group>
       <ActionIcon size="lg" variant="default" onClick={handleLogout}>
         <IconLogout size={20} />
