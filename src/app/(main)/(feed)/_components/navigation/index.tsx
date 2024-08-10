9e112ba897e1dab28debbf2e141062951e2d152d
Maxim S <deed34@yandex.ru>
2024-08-10 20:15:51 +0300

    refactor: feed navigation

--- src/app/(main)/(feed)/_components/navigation/index.tsx
+++ src/app/(main)/(feed)/_components/navigation/index.tsx
@@ -3,72 +3,60 @@
 import { Anchor, Card, NavLink, Stack, Text } from "@mantine/core";
 import { LogoIcon } from "@ui/icons";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
+import { ParsedUrlQueryInput } from "querystring";
 import type { JSX } from "react";
 
 import styles from "./styles.module.scss";
 
+const categories = [
+  { id: "", label: "🏠 Home" },
+  { id: "podcasts", label: "🎙️ Podcasts" },
+  { id: "development", label: "👨‍💻 Software Development" },
+  { id: "politics", label: "🏛️ Politics" },
+  { id: "news", label: "🌎 World News" },
+  { id: "sports", label: "⚽ Sports" },
+];
+
 export const Navigation = (): JSX.Element => {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   return (
     <Stack className={styles.wrapper}>
       <Stack mb="sm" gap="xs">
-        <NavLink
-          component={Link}
-          href="/"
-          label="🏠 Home"
-          classNames={{
-            root: styles.navlink,
-            label: styles.navlinkLabel,
-          }}
-          active={pathname == "/"}
-        />
-        <NavLink
-          component={Link}
-          href="/"
-          label="🎙️ Podcasts"
-          classNames={{
-            root: styles.navlink,
-            label: styles.navlinkLabel,
-          }}
-        />
-        <NavLink
-          component={Link}
-          href="/"
-          label="👨‍💻 Software Development"
-          classNames={{
-            root: styles.navlink,
-            label: styles.navlinkLabel,
-          }}
-        />
-        <NavLink
-          component={Link}
-          href="/"
-          label="🏛️ Politics"
-          classNames={{
-            root: styles.navlink,
-            label: styles.navlinkLabel,
-          }}
-        />
-        <NavLink
-          component={Link}
-          href="/"
-          label="🌎 World News"
-          classNames={{
-            root: styles.navlink,
-            label: styles.navlinkLabel,
-          }}
-        />
-        <NavLink
-          component={Link}
-          href="/"
-          label="⚽ Sports"
-          classNames={{
-            root: styles.navlink,
-            label: styles.navlinkLabel,
-          }}
-        />
+        {categories.map(({ id, label }) => {
+          const queryParamName = "category";
+
+          return (
+            <NavLink
+              key={id}
+              component={Link}
+              href={{
+                query: id && {
+                  ...Array.from(searchParams.entries()).reduce(
+                    (memo: ParsedUrlQueryInput, [key, value]) => {
+                      memo[key] = value;
+
+                      return memo;
+                    },
+                    {}
+                  ),
+                  [queryParamName]: id,
+                },
+                pathname,
+              }}
+              label={label}
+              classNames={{
+                root: styles.navlink,
+                label: styles.navlinkLabel,
+              }}
+              active={
+                searchParams.has(queryParamName, id) || (!id && !searchParams.has(queryParamName))
+              }
+            />
+          );
+        })}
       </Stack>
 
       <Card padding="sm" radius="md">
