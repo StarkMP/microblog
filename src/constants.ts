722f00180257f35b390a0fc110e52cc6eca4123b
Maxim S <deed34@yandex.ru>
2024-08-12 19:28:14 +0300

    refactor: move default get revalidate next fetch cache param to separate constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
-// Cookies
+// Cookie names
 export const ACCESS_TOKEN_COOKIE_NAME = "accessToken" as const;
 export const REFRESH_TOKEN_COOKIE_NAME = "refreshToken" as const;
 
 // Numbers
 export const ACCESS_TOKEN_EXPIRES_IN_MINS = 60 as const;
+export const DEFAULT_GET_DATA_REVALIDATION_TIME = 10 as const;
