403b1d6ba22dc0862b348b6e1173c6cc4f268728
Maxim S <deed34@yandex.ru>
2024-08-14 18:16:57 +0300

    refactor: authorization modals & parallel routes flow

--- src/app/(main)/_components/header/auth-buttons/index.tsx
+++ src/app/(main)/_components/header/auth-buttons/index.tsx
@@ -1,25 +1,34 @@
 "use client";
 
+import { LoginModal, SignUpModal } from "@features";
 import { Button, Group } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { IconLogin2 } from "@tabler/icons-react";
-import Link from "next/link";
 import { type JSX } from "react";
 
-export const AuthorizationButtons = (): JSX.Element => (
-  <Group gap="xs">
-    <Button
-      component={Link}
-      scroll={false}
-      href="/login"
-      prefetch
-      variant="light"
-      radius="md"
-      leftSection={<IconLogin2 size={20} />}
-    >
-      Sign In
-    </Button>
-    <Button component={Link} scroll={false} href="/signup" prefetch variant="default" radius="md">
-      Create account
-    </Button>
-  </Group>
-);
+export const AuthorizationButtons = (): JSX.Element => {
+  const [loginModalOpened, { close: closeLoginModal, open: openLoginModal }] = useDisclosure(false);
+  const [signUpModalOpened, { close: closeSignUpModal, open: openSignUpModal }] =
+    useDisclosure(false);
+
+  return (
+    <>
+      <Group gap="xs">
+        <Button
+          variant="light"
+          radius="md"
+          leftSection={<IconLogin2 size={20} />}
+          onClick={openLoginModal}
+        >
+          Sign In
+        </Button>
+        <Button variant="default" radius="md" onClick={openSignUpModal}>
+          Create account
+        </Button>
+      </Group>
+
+      <LoginModal opened={loginModalOpened} onClose={closeLoginModal} />
+      <SignUpModal opened={signUpModalOpened} onClose={closeSignUpModal} />
+    </>
+  );
+};
